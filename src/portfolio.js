cc0f37f41714a797df4eb3eec9a20eb4b1d9cb06
Omoba, George <georgedevlin67@gmail.com>
2021-09-04 23:24:28 +0100

    portfolio update

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,14 +1,14 @@
 import emoji from 'react-easy-emoji';
 
+import Nubia from './assets/img/icons/common/Nubia.png'
 import Edrop from './assets/img/icons/common/Edrop.png'
 import PalmTaxy from './assets/img/icons/common/PalmTaxy.png'
-import TC from './assets/img/icons/common/TC.png'
-
+ 
 export const greetings = {
     "name": "Omoba George",
     "title": "Hey there, I'm George.",
-    "description": "A passionate IT Enthusiast with skills in Product Management, Front-End Web Developement, Network/Hardware Maintenance Engineering, Data, Software Testing/Management and lately Blockchain Development. Having an experience building and handling teams in building Websites, Web and Mobile Apps with JavaScript / Reactjs / Nodejs / Python / Django / Flask / C++ / PHP and other cool libraries, frameworks and Cross Platform Mobile Apps. 'A huge Linux OS Fan'.",
-    "resumeLink": "https://drive.google.com/file/d/1O4XVEH6PWTJ1UIhlWaw-ALlNIef4xJjh/view?usp=sharing"
+    "description": "A passionate IT Enthusiast with skills in Blockchain Smart Contracts, Product Management/Design, Front-End Web Developement, Network/Hardware Maintenance, Data, Software Management. Having an experience building and handling teams in building Websites, Web and Mobile Apps with JavaScript / Reactjs / Nodejs / Python / Django / Flask / C++ / PHP across other libraries and frameworks. 'A Linux OS Administrator'.",
+    "resumeLink": "https://drive.google.com/file/d/1MVwOG0qp5pul1I4LgBUDRyXtGzEOXfwk/view?usp=sharing"
 }
 
 export const openSource = {
@@ -20,7 +20,7 @@ export const contact = {
 }
 
 export const socialLinks = {
-    "facebook": "https://www.facebook.com/george.devlin.144",
+    "facebook": "#",
     "instagram": "https://www.instagram.com/omoba_devlin?r=nametag",
     "twitter": "#",
     "github": "https://github.com/omobadev",
@@ -29,14 +29,14 @@ export const socialLinks = {
 
 export const skillsSection = {
     title: 'What I do',
-    subTitle: 'INNOVATIVE TECH ENTHUSIAST / DEVELOPER / SKETCH ARTIST WHO WANTS TO EXPLORE EVERYTHING DESIGN AND PRODUCTION',
+    subTitle: 'INNOVATIVE TECH ENTHUSIAST / DEVELOPER / SKETCH ARTIST WHO EXPLORES EVERYTHING DESIGN AND PRODUCTION',
     skills: [
       emoji(
-        '⚡ Develop highly innovative Products / Applications'
+        '⚡ Development of Highly Innovative Products'
       ),
-      emoji('⚡ Progressive Network and Hardware Maintenance / Repair and Upgrade'),
+      emoji('⚡ BlockChain Smart Contracts Developement / Audit'),
       emoji(
-        '⚡ Integration of all Modern Day technology as I take on New Experimental Projects'
+        '⚡ IT Systems Maintenance'
       ),
     ],
     
@@ -91,11 +91,11 @@ export const skillsSection = {
 
 export const SkillBars = [
     {
-      Stack: 'Product Management/Design', //Insert stack or technology you have experience in
+      Stack: 'Product Design/Management', //Insert stack or technology you have experience in
       progressPercentage: '80', //Insert relative proficiency in percentage
     },
     {
-      Stack: 'IT Network/Hardware Engineering',
+      Stack: 'BlockChain Developement',
       progressPercentage: '70',
     },
     {
@@ -108,7 +108,7 @@ export const SkillBars = [
     },
     {
       Stack: 'Sketch Art',
-      progressPercentage: '60',
+      progressPercentage: '70',
     },
   ]
 
@@ -118,19 +118,27 @@ export const educationInfo = [
       subHeader: 'Bachelor of Science in Computer Science',
       duration: 'September 2013 - October 2017',
       desc:
-        'Ranked top 10% in the program. Took courses on Software Engineering, Artificial Intelligence, Web Security, Operating Systems, Etc.',
+        'Advanced Applied Siences Program. With courses on Software Engineering, Artificial Intelligence, Web Security, Operating Systems, Etc.',
       descBullets: ['Project work on the Hadoop Framework Distribution System.'],
     },
 ]
 
 export const experience = [
   {
-    role: 'Technical Project/Product Manager',
+    role: 'Support Blockchain Developer',
+    company: 'Nubia Inc',
+    companylogo: Nubia,
+    date: 'Nov 2020 – Present',
+    desc:
+      'Developing, Deploying and Auditing Blockchain Smart Contracts (Solidity)',
+  },
+  {
+     role: 'Technical Project/Product Manager',
     company: 'Edrop Co',
     companylogo: Edrop,
     date: 'Nov 2019 – April 2020',
     desc:
-      'Generally over seeing all processes throughout product lifecycle. Working closely with a team of UI/UX Designer, Front/Back End developers, DevOps Engineer, Content Creator/Social media manager. To achieve a fully functional product.',
+      'Generally over seeing all processes throughout product lifecycle. Working with a team of UI/UX Designer, Front/Back End developers, DevOps Engineer, Content Creator/Social media manager. To achieve a fully functional product.',
   },
   {
     role: 'Product Manager',
@@ -138,29 +146,21 @@ export const experience = [
     companylogo: PalmTaxy,
     date: 'May 2019 – Sep 2019',
     desc:
-      'Regular maintenance and troubleshooting of the mobile application. Working with the software developers in solving all customer related issues Drivers/Riders.',  
-  },
-  {
-    role: 'Front-End Developer',
-    company: 'Riv TechCreek',
-    companylogo: TC,
-    date: 'Sep 2018 – March 2019',
-    desc:
-      'joining a team of Front-End developers, UI/UX Designers, Back-End developers and Dev-Ops Engineers to build a self learning Patient database for all Government Hospitals in the State.',
+      'Regular troubleshooting, maintenance and configuration of the PalmTaxy Taxi hailing mobile application. Working with the software developers in solving all customer related issues for Drivers and Riders.',
   },
 ]
 
 export const projects = [
   {
     name: "CayWoodBrown Foundation",
-    desc: "Lectured the youths on the basics of web design. CayWoodBrown is a Foundation started by the deputy Governor of Rivers State, in 2018 to Educate the youths on a few ICT Skills. ",
+    desc: "Youth Sensitisation of The Usefulness of Web Developement and Digital Marketing. CayWoodBrown is a Foundation created by the Deputy Governor of Rivers State, Nigeria, in 2018 to Educate the youths on a few ICT Skills. ",
   },
   {
-    name: "Riv TechCreek",
-    desc: "Part of a team tasked to build a group-chat mobile application",
+    name: "Nubia",
+    desc: "TRC20 Open Source Peer to Peer Digital Currency ",
     link: {
-      name: "George",
-      url: "https://www.linkedin.com/in/george-omoba-3a957a93"
+      name: "Nubia OGD",
+      url: "https://tronscan.org/#/token20/TW8SB94JtoPvKnzd17rLiUZcW9ayuEKuVv"
     }
   }
 ]
